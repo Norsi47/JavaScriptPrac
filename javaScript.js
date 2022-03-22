a66aa2b000264f6634059cfeffffa56edbd81835
Norsi <briggsnorsi@yahoo.com>
2022-03-21 22:29:43 -0400

    First JavaScript Practice

---
+++ javaScript.js
@@ -0,0 +1,53 @@
+let message;
+
+message = 'Hello';
+
+alert(message);
+
+
+//can also be done this way
+let message2 = 'Hello there';
+alert(message2);
+
+//adding user
+let user = 'Norsi';
+ let age = 25;
+ let message3 = 'Hello';
+
+
+ let hello = 'Hello World!';
+ let message4;
+ message4 = hello;
+
+ alert (hello);
+ alert(message4);
+
+ //using both variables
+ let $ = 1;
+ let _ = 2;
+
+ alert($ + _);
+
+ //use const for unchanging variables
+ const myBirthDay = '12.12.1997';
+ alert(myBirthDay);
+
+ //using colors
+ const COLOR_RED = "#F00";
+ const COLOR_GREEN = "#0F0";
+ const COLOR_BLUE = "#00F";
+ const COLOR_ORANGE = "#FF&F00";
+
+ let color = COLOR_GREEN;
+ alert(color);
+
+ //tasks to do
+ //assign john to admin
+let admin;
+let name = 'John';
+admin = name ;
+alert (admin);
+
+//task 2
+const PLANET = "EARTH";
+const CURRENTVISITOR = "Yammy";
