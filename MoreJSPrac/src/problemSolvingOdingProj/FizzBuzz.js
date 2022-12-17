522985629a44d681fbd84801d6c29b0455b5a64e
Norsi47 <briggsnorsi@yahoo.com>
2022-12-16 23:09:24 -0500

    adding for loop and if statements for users entered number

--- MoreJSPrac/src/problemSolvingOdingProj/FizzBuzz.js
+++ MoreJSPrac/src/problemSolvingOdingProj/FizzBuzz.js
@@ -1,2 +1,17 @@
 //asking user to input number
 let usersAnswer = parseInt(prompt("Enter the number you would like to FizzBuzz up to: "));
+
+//for loop to loop from 1 till entered number
+//usally starts from 0 (i = 0) but starting from 1 at this point due to the need of the game being programmed
+for(i = 1; i <= usersAnswer; i++) {
+    //if number is divisible by 3 (no remainder)print fizz
+   /* use % to let us know if there is a remainder or not,
+   * is the remainder of 3 = to 0? if so print fizz*/
+    if(i % 3 === 0) {
+        console.log("Fizz");
+    } //else just print out number
+    else {
+        console.log(i);
+    }
+
+}
