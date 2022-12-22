e055fca641464e59c6ffa8c93aa6f46600a862c3
Norsi47 <briggsnorsi@yahoo.com>
2022-12-22 15:45:17 -0500

    for streak

--- MoreJSPrac/src/problemSolvingOdingProj/FizzBuzz.js
+++ MoreJSPrac/src/problemSolvingOdingProj/FizzBuzz.js
@@ -3,7 +3,13 @@ let usersAnswer = parseInt(prompt("Enter the number you would like to FizzBuzz u
 
 //for loop to loop from 1 till entered number
 //usally starts from 0 (i = 0) but starting from 1 at this point due to the need of the game being programmed
+/**/
 for(i = 1; i <= usersAnswer; i++) {
+
+    //if the number is divisible by 3 or 5
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    }
     //if number is divisible by 3 (no remainder)print fizz
    /* use % to let us know if there is a remainder or not,
    * is the remainder of 3 = to 0? if so print fizz*/
