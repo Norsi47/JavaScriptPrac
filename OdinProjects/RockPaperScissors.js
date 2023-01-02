6dc4d09213bc3634028bd280a6afed49cb33d4c1
Norsi47 <briggsnorsi@yahoo.com>
2023-01-01 23:17:12 -0500

    3 ways to display message in page using js

---
+++ OdinProjects/RockPaperScissors.js
@@ -0,0 +1,10 @@
+//ways to show message in page
+
+console.log("Hello World");
+
+alert("Hello World_using alert");
+
+/*
+method to write message in <body>
+ instead of doing it in html*/
+document.write("Hello World_Using document.write method");
