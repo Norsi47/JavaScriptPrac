0284485b23661abcfbdd48449595fc859277cde2
Norsi47 <briggsnorsi@yahoo.com>
2023-01-01 23:32:13 -0500

    testing with different elements

--- OdinProjects/RockPaperScissors.js
+++ OdinProjects/RockPaperScissors.js
@@ -8,3 +8,12 @@ alert("Hello World_using alert");
 method to write message in <body>
  instead of doing it in html*/
 document.write("Hello World_Using document.write method");
+
+/*picks the element and writes message in there
+* for example "body" is the element, <body>
+element can be anything in html format*/
+let htmlBody = document.querySelector("p");
+htmlBody.innerText = "Testing using document.querySelector";
+//or
+let htmlHeader =document.querySelector("h1")
+htmlHeader.innerHTML = "Header h1 - innerHTML";
