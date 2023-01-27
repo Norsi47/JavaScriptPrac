e4aae15bc98f279e0242af257992113a1ffad80e
Norsi47 <briggsnorsi@yahoo.com>
2023-01-26 22:13:24 -0500

    more exmaples on using arrays

--- FundamentalsP4/hello.js
+++ FundamentalsP4/hello.js
@@ -1,8 +1,20 @@
 /*Different ways to use arrays
 * Good prac to use const*/
-const cars = ["GMC", "Chevy", "Toyota"];
+const cars = ["GMC", "Chevy", "Toyota", "Honda", "Corvette", "Lexus"];
 //can pick which one to print out
-console.log(cars[2], ", Array Length of Cars:", cars.length)
+console.log(cars[2], ", Array Length of Cars:", cars.length);
+//accessing last string in array
+console.log( "Car at the end" ,cars[cars.length - 1]);
+
+//using for loop for arrays
+let carsLength = cars.length;
+let text = "<ul>";
+console.log("<-----For Loop for all Cars--->");
+for (let i = 0; i < carsLength; i++) {
+    console.log("<li>" +cars[i] + "</li>");
+}
+//print this after open bracket is closed
+text += "</ul>";
 
 const moreCars = [];
 moreCars [0] = "Honda";
@@ -17,6 +29,7 @@ console.log("Printing all cars ->" +moreCars);
 const usersInfo = {firstName: "Felix", lastName: "Alvez", age: 26};
 let userLength = usersInfo.length;
 console.log("Users Fname and Lname-> " + usersInfo.firstName, usersInfo.lastName + " Length of Array " + userLength);
+
 
 
 
