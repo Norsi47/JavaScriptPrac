10f52432a182d21cf62f7128f365e9ef48cdfd6f
Norsi47 <briggsnorsi@yahoo.com>
2023-01-27 22:10:25 -0500

    more functions to use when using arrays

--- FundamentalsP4/hello.js
+++ FundamentalsP4/hello.js
@@ -30,6 +30,26 @@ const usersInfo = {firstName: "Felix", lastName: "Alvez", age: 26};
 let userLength = usersInfo.length;
 console.log("Users Fname and Lname-> " + usersInfo.firstName, usersInfo.lastName + " Length of Array " + userLength);
 
+const fruitsNames = ["Banana", "Apple", "Tomatoes", "Oranges", "Tangerines"];
+//separates arrays with commas
+console.log("Fruit Names=> " + fruitsNames.toString());
+// separates with what is in ()
+console.log(fruitsNames.join("==") + "\nUsing pop() " + fruitsNames.pop() ,
+"\n using push()", fruitsNames.push("Grapes") + " After using Push() " + fruitsNames);
+console.log("Shift() "+ fruitsNames.shift() , "Using Delete-> ", delete fruitsNames[2] + "\n After using delete " + fruitsNames[2]);
+
+//combining two arrays
+const firstNames = ["Norsi", "Arleen", "Landerson", "Hector"];
+const lastNames = ["Onyekaba", "Patinga", "Chile", "Vance"];
+const combineFnameLname = firstNames.concat(lastNames);
+console.log(combineFnameLname);
+const addingUser = firstNames.concat("Boma");
+console.log(addingUser);
+lastNames.splice(2, 3 ,"Yamaha", "FisherMan");
+console.log("After using splice", lastNames);
+
+
+
 
 
 
